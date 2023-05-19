7878bc4a17ea98fab0fcac65022236e6f49c973f
IvanZhdaniuk <iz2819815@gmail.com>
2023-05-19 15:48:04 +0300

    #3 Created models and admin

--- pizzashopapp/admin.py
+++ pizzashopapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pizza, Soup, Other, Order, Size
 
 # Register your models here.
+admin.site.register(Pizza)
+admin.site.register(Soup)
+admin.site.register(Other)
+admin.site.register(Order)
+admin.site.register(Size)
--- pizzashopapp/models.py
+++ pizzashopapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 
@@ -10,8 +12,34 @@ class Pizza(models.Model):
     price = models.FloatField()
     photo = models.ImageField(upload_to='pizzashopapp_photo_pizza/', blank=False)
     rating = models.FloatField()
-    def __str__(self):
+
+    def __unicode__(self):
         return self.name
+
+class Size(models.Model):
+    SIZE_CHOICES = (
+        ('option1','smol'),
+        ('option2','normal'),
+        ('option3','big'),
+    )
+    size = models.CharField(max_length=20, choices=SIZE_CHOICES)
+    pizza = models.ManyToManyField(Pizza, through='PizzaSize', related_name='size')
+
+
+    def __str__(self):
+        return f'{self.size} - {self.pk}'
+
+class PizzaSize(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+
+
+    class Meta(object):
+        unique_together = ('pizza', 'size')
+
+    def __str__(self):
+        return f'{self.pizza}  {self.size}'
+
 
 class Soup(models.Model):
     name = models.CharField(max_length=100)
@@ -45,6 +73,16 @@ class Other(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=20)
+    total_amount = models.DecimalField(auto_created=True, max_digits=10, decimal_places=2)
+    order_description = models.TextField(max_length=300)
+
+
+    def __str__(self):
+        return self.order_number
 
 
 
